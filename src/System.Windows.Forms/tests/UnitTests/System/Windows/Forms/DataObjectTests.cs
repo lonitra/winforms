cbad2188f7f5f3e3f10acb84315e711fb60a29ad
Loni Tra <lonitra@microsoft.com>
2024-10-22 17:43:41 -0700

    use Single() and add Font to test type

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/DataObjectTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/DataObjectTests.cs
@@ -2579,11 +2579,13 @@ public partial class DataObjectTests
     {
         // This test demonstrates one way users can achieve custom JSON serialization behavior if the
         // default JSON serialization behavior that is used in SetDataAsJson APIs is not enough for their scenario.
+        Font font = new("Consolas", emSize: 10);
         WeatherForecast forecast = new()
         {
             Date = DateTimeOffset.Now,
             TemperatureCelsius = 25,
-            Summary = "Hot"
+            Summary = "Hot",
+            Font = font
         };
 
         DataObject dataObject = new();
@@ -2593,6 +2595,7 @@ public partial class DataObjectTests
         deserialized.Date.ToString(offsetFormat).Should().Be(forecast.Date.ToString(offsetFormat));
         deserialized.TemperatureCelsius.Should().Be(forecast.TemperatureCelsius);
         deserialized.Summary.Should().Be($"{forecast.Summary} custom!");
+        deserialized.Font.Should().Be(font);
     }
 
     [JsonConverter(typeof(WeatherForecastJsonConverter))]
@@ -2601,6 +2604,7 @@ public partial class DataObjectTests
         public DateTimeOffset Date { get; set; }
         public int TemperatureCelsius { get; set; }
         public string Summary { get; set; }
+        public Font Font { get; set; }
     }
 
     private class WeatherForecastJsonConverter : JsonConverter<WeatherForecast>
@@ -2608,10 +2612,18 @@ public partial class DataObjectTests
         public override WeatherForecast Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             WeatherForecast result = new();
+            string fontFamily = null;
+            int size = -1;
             while (reader.Read())
             {
                 if (reader.TokenType == JsonTokenType.EndObject)
                 {
+                    if (fontFamily is null || size == -1)
+                    {
+                        throw new JsonException();
+                    }
+
+                    result.Font = new(fontFamily, size);
                     return result;
                 }
 
@@ -2635,6 +2647,12 @@ public partial class DataObjectTests
                     case nameof(WeatherForecast.Summary):
                         result.Summary = reader.GetString();
                         break;
+                    case nameof(Font.FontFamily):
+                        fontFamily = reader.GetString();
+                        break;
+                    case nameof(Font.Size):
+                        size = reader.GetInt32();
+                        break;
                     default:
                         throw new JsonException();
                 }
@@ -2649,6 +2667,8 @@ public partial class DataObjectTests
             writer.WriteString(nameof(WeatherForecast.Date), value.Date.ToString("MM/dd/yyyy"));
             writer.WriteNumber(nameof(WeatherForecast.TemperatureCelsius), value.TemperatureCelsius);
             writer.WriteString(nameof(WeatherForecast.Summary), $"{value.Summary} custom!");
+            writer.WriteString(nameof(Font.FontFamily), value.Font.FontFamily.Name);
+            writer.WriteNumber(nameof(Font.Size), value.Font.Size);
             writer.WriteEndObject();
         }
     }
